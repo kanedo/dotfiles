62d00bc910694181bd5100b12c73e32229dc1003
Gabriel Bretschner <software@kanedo.net>
2015-02-11 11:46:22 +0100

    use variable for defining dotfiles directory

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
-cd $HOME
-git clone https://github.com/kanedo/dotfiles.git
-ln -s $HOME/dotfiles/.zsh $HOME
-ln -s $HOME/dotfiles/.zshrc $HOME
-ln -s $HOME/dotfiles/.vim $HOME
-ln -s $HOME/dotfiles/.vimrc $HOME
+#!/bin/sh
+DOTFILES_DIR=$HOME/dotfiles
+
+git clone https://github.com/kanedo/dotfiles.git $DOTFILES_DIR
+ln -s $DOTFILES_DIR/.zsh $HOME
+ln -s $DOTFILES_DIR/.zshrc $HOME
+ln -s $DOTFILES_DIR/.vim $HOME
+ln -s $DOTFILES_DIR/.vimrc $HOME
 mkdir $HOME/.vimbackup
 
 #sudo ln -s $HOME/dotfiles/.vim /root
 #sudo ln -s $HOME/dotfiles/.vimrc /root
 #sudo mkdir /root/.vimbackup
 
-cd dotfiles
+cd $DOTFILES_DIR
 git submodule init
 git submodule update --recursive
 
