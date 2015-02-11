f59bb4202f47725068b2d0d065bcf6f234d930b5
Gabriel Bretschner <software@kanedo.net>
2015-02-11 11:33:40 +0100

    added simple install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd $HOME
+git clone https://github.com/kanedo/dotfiles.git
+ln -s $HOME/dotfiles/.zsh $HOME
+ln -s $HOME/dotfiles/.zshrc $HOME
+ln -s $HOME/dotfiles/.vim $HOME
+ln -s $HOME/dotfiles/.vimrc $HOME
+mkdir $HOME/.vimbackup
+
+#sudo ln -s $HOME/dotfiles/.vim /root
+#sudo ln -s $HOME/dotfiles/.vimrc /root
+#sudo mkdir /root/.vimbackup
+
+cd dotfiles
+git submodule init && git submodule update —recursive
