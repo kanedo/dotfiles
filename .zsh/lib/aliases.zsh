110088953cf06880faef912b88100ad9580dbd58
Gabriel Bretschner <software@kanedo.net>
2015-02-11 13:17:32 +0100

    moved autocompletion for aliases

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -3,11 +3,8 @@ alias lh='ls -lah'
 alias ...='cd ../..'
 
 alias gs='git status'
-compdef _git gs=git-status
 alias gc='git commit'
-compdef _git gc=git-commit
 alias ga='git add'
-compdef _git ga=git-add
 
 function man-preview() {
   man -t "$@" | open -f -a Preview
--- .zsh/lib/directories.zsh
+++ .zsh/lib/directories.zsh
@@ -2,3 +2,15 @@
 setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
+
+function pfd() {
+  osascript 2>/dev/null <<EOF
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell
+EOF
+}
+
+function cdf() {
+  cd "$(pfd)"
+}
