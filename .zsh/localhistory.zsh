97fdca1e495eb5931a08e0012714799805902573
Gabriel Bretschner <software@kanedo.net>
2019-03-07 14:39:49 +0100

    moved local history use zsh's autoload to add it to precmd

--- .zsh/localhistory.zsh
+++ .zsh/localhistory.zsh
@@ -1,18 +1,17 @@
 alias useHistory="grep -v \"^ls$\|^ll$\|^l$\|^dir$\|^cd \|^h$\|^gh$\|^gh \|^bg$\|^fg$\|^qsm$\|^quser$\|^qstat \|^note \|^mutt\|^std \" | wc -l"
-                     
+
 function myLocalHistory()
 {
     if [[ `ls -ld "$PWD" | awk '{print $3}'` == "$USER" ]] ; then
         HISTORYLINE=`history | tail -1 | sed 's:^ *[0-9]* *::g'`
         if [[ "$(echo $HISTORYLINE | useHistory | tr -d '[:space:]')" == "1" ]] ; then
-            ((((date +%F.%H-%M-%S.;uname -n) | tr -d '\n' ; echo " $HISTORYLINE") >>.history) 2>/dev/null &) 
+            ((((date +%F.%H-%M-%S.;uname -n) | tr -d '\n' ; echo " $HISTORYLINE") >>.history) 2>/dev/null &)
         fi
     fi
 }
 
-if [[ ! "$PROMPT_COMMAND" == *"myLocalHistory"* ]]; then
-    export PROMPT_COMMAND="myLocalHistory $PROMPT_COMMAND"
-fi
+autoload -U add-zsh-hook
+add-zsh-hook precmd myLocalHistory
 
 function h()
 {
