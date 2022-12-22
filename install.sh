d5bf369956787afcc6f4df31424b3d2a53375a2f
Gabriel Bretschner <gabriel@lilt.com>
2022-12-22 08:28:19 +0000

    checkout main

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 DOTFILES_DIR=$HOME/dotfiles
 
 git clone https://github.com/kanedo/dotfiles.git $DOTFILES_DIR
+git checkout main
 ln -s $DOTFILES_DIR/.zshrc $HOME
 ln -s $DOTFILES_DIR/.vimrc $HOME
 ln -s $DOTFILES_DIR/.tmux.conf $HOME
