b983c93607ecf66b348d10b529f1c6c22f3b2725
Gabriel Bretschner <gabriel@lilt.com>
2022-12-22 08:29:36 +0000

    force link

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ DOTFILES_DIR=$HOME/dotfiles
 
 git clone https://github.com/kanedo/dotfiles.git $DOTFILES_DIR
 git checkout main
-ln -s $DOTFILES_DIR/.zshrc $HOME
-ln -s $DOTFILES_DIR/.vimrc $HOME
-ln -s $DOTFILES_DIR/.tmux.conf $HOME
-ln -s $DOTFILES_DIR/.tmux $HOME
-ln -s $DOTFILES_DIR/.global_ignore $HOME
+ln -fs $DOTFILES_DIR/.zshrc $HOME
+ln -fs $DOTFILES_DIR/.vimrc $HOME
+ln -fs $DOTFILES_DIR/.tmux.conf $HOME
+ln -fs $DOTFILES_DIR/.tmux $HOME
+ln -fs $DOTFILES_DIR/.global_ignore $HOME
 mkdir $HOME/.vimbackup
 
 # Install oh-my-zsh
