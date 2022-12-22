161aeeeaabef6f6a4da12c5263d1fff5df80d541
Gabriel Bretschner <gabriel@lilt.com>
2022-12-22 08:38:19 +0000

    omz

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 DOTFILES_DIR=$HOME/dotfiles
 
 git clone https://github.com/kanedo/dotfiles.git $DOTFILES_DIR
-git checkout main
+# Install oh-my-zsh
+rm -rf $HOME/.oh-my-zsh/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm $HOME/.zshrc
 ln -fs $DOTFILES_DIR/.zshrc $HOME
 ln -fs $DOTFILES_DIR/.vimrc $HOME
 ln -fs $DOTFILES_DIR/.tmux.conf $HOME
@@ -10,8 +13,5 @@ ln -fs $DOTFILES_DIR/.tmux $HOME
 ln -fs $DOTFILES_DIR/.global_ignore $HOME
 mkdir $HOME/.vimbackup
 
-# Install oh-my-zsh
-rm -rf $HOME/.oh-my-zsh/
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git config --global core.excludesfile ~/.global_ignore
