dd5ab17b3a47d0ecb8f84c1da9a50524d4320741
Gabriel Bretschner <gabriel@lilt.com>
2023-01-09 09:34:50 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 DOTFILES_DIR=$HOME/dotfiles
 
-git clone https://github.com/kanedo/dotfiles.git $DOTFILES_DIR
+git clone --recurse-submodules https://github.com/kanedo/dotfiles.git $DOTFILES_DIR
 # Install starship
 curl -sS https://starship.rs/install.sh | sh
 
 # Install oh-my-zsh
 rm -rf $HOME/.oh-my-zsh/
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm $HOME/.zshrc
 ln -fs $DOTFILES_DIR/.zshrc $HOME
 ln -fs $DOTFILES_DIR/.vimrc $HOME
 ln -fs $DOTFILES_DIR/.tmux.conf $HOME
 ln -fs $DOTFILES_DIR/.tmux $HOME
 ln -fs $DOTFILES_DIR/.global_ignore $HOME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+rm -rf $HOME/.oh-my-zsh/custom/
+ln -fs $DOTFILES_DIR/.oh-my-zsh/custom $HOME/.oh-my-zsh/custom
 mkdir $HOME/.vimbackup
 
 
